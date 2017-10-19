0322f82a72332a01196c3e3c698c1bb76acea0ac
mzheryagin <zheryagin@gmail.com>
2017-10-19 22:57:07 +0300

    Point class done. Need to check. Maybe add test.

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,24 @@
+public class BruteCollinearPoints {
+
+  /**
+   * finds all line segments containing 4 points
+   * @param points
+   */
+  public BruteCollinearPoints(Point[] points) {
+
+  }
+
+  /**
+   * the number of line segments
+   */
+  public int numberOfSegments() {
+
+  }
+
+  /**
+   * // the line segments
+   */
+  public LineSegment[] segments() {
+
+  }
+}
