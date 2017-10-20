f4d8584183cd706004bc12314734cc01fc5e2847
mzheryagin <zheryagin@gmail.com>
2017-10-20 18:03:33 +0300

    Working on BruteCollinearPoints class

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,10 +1,22 @@
+import java.util.Arrays;
 public class BruteCollinearPoints {
 
   /**
    * finds all line segments containing 4 points
+   * Throw a java.lang.IllegalArgumentException if the argument to the constructor is null,
+   * if any point in the array is null, or if the argument to the constructor contains a repeated point.
    * @param points
    */
   public BruteCollinearPoints(Point[] points) {
+    if (points == null) {
+      throw new java.lang.IllegalArgumentException();
+    }
+    Arrays.sort(points);
+    for (int i = 1; i < points.length; i++) {
+      if (points[i-1].compareTo(points[i]) == 0) {
+        throw new java.lang.IllegalArgumentException();
+      }
+    }
 
   }
 
